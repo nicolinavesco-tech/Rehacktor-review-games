a18831e410e69e9c8ff109b38c1c98464f299804
Nicolina <nicolina.vesco@gmail.com>
2026-03-24 21:37:40 +0100

    Spostate le immagini della prima section su asset

--- src/view/Homepage.jsx
+++ src/view/Homepage.jsx
@@ -4,6 +4,11 @@ import TopGamesList from "../components/homeComponents/TopGamesList";
 import TopGamesCard from "../components/homeComponents/TopGamesCard";
 import UpcomingGamesCard from "../components/homeComponents/UpcomingGamesCard";
 import UpcomingGamesList from "../components/homeComponents/UpcomingGamesList";
+import godOfWar from "../assets/god.jpg";
+import gta from "../assets/gta.jpg";
+import ghost from "../assets/ghost.avif";
+import prince from "../assets/prince.jpg";
+import readDead from "../assets/readdead.jpg";
 
 export default function Homepage() {
     const { games, topRated, upcoming } = useLoaderData();
@@ -29,7 +34,7 @@ export default function Homepage() {
 
                             <h2 className="text-xs uppercase tracking-wider text-(--color-btn) pt-6 font-semibold">Ultime Notizie</h2>
                             <div className="border-b border-white/10 pb-4 flex gap-4 items-center">
-                                <img src="/public/media/red_dead_redemption_II.jpg" alt="" className="w-[110px] h-[80px] rounded-lg flex-shrink-0" />
+                                <img src={godOfWar} alt="God of War" className="w-[110px] h-[80px] rounded-lg flex-shrink-0" />
                                 <div className="flex flex-col">
 
                                     <Link to=""><h3 className="font-bold text-lg md:text-xl leading-tight mt-1">Red Dead Redemption torna a far parlare di sé: nuovi dettagli sul possibile remake</h3></Link>
@@ -37,21 +42,21 @@ export default function Homepage() {
                                 </div>
                             </div>
                             <div className="border-b border-white/10 pb-4 flex gap-4 items-center">
-                                <img src="/public/media/gta.jpg" alt="" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
+                                <img src={gta} alt="Gta" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
                                 <div className="flex flex-col">
                                     <Link to=""><h3 className="font-bold text-lg md:text-xl leading-tight mt-1">GTA VI promette un mondo di gioco enorme: emergono nuovi dettagli sul gameplay</h3></Link>
                                     <p className="text-sm text-gray-300 mt-2 leading-6">Il prossimo capitolo della serie Grand Theft Auto punta a ridefinire il genere open world. Le ultime informazioni parlano di una mappa..</p>
                                 </div>
                             </div>
                             <div className="border-b border-white/10 pb-4 flex gap-4 items-center">
-                                <img src="/public/media/ghost.avif" alt="" className="w-[110px] h-[80px]  rounded-lg flex-shrink-0" />
+                                <img src={ghost} alt="Ghost of Tsushima" className="w-[110px] h-[80px]  rounded-lg flex-shrink-0" />
                                 <div className="flex flex-col">
                                     <Link to=""><h3 className="font-bold text-lg md:text-xl leading-tight mt-1">Ghost of Tsushima continua a conquistare i giocatori: successo anche su nuove piattaforme</h3></Link>
                                     <p className="text-sm text-gray-300 mt-2 leading-6">L'avventura samurai di Sucker Punch rimane uno dei titoli più apprezzati degli ultimi anni. Tra combattimenti spettacolari e ambientazioni mozzafiato..</p>
                                 </div>
                             </div>
                             <div className="border-b border-white/10 pb-4 flex gap-4 items-center">
-                                <img src="/public/media/god_of_War.jpg" alt="" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
+                                <img src={prince} alt="Prince of Persia" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
                                 <div className="flex flex-col">
 
 
@@ -60,7 +65,7 @@ export default function Homepage() {
                                 </div>
                             </div>
                             <div className="border-b-0 border-white/10 pb-4 flex gap-4 items-center">
-                                <img src="/public/media/prince-of-persia.jpg" alt="" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
+                                <img src={readDead} alt="Read Dead" className="w-[110px] h-[80px] object-cover rounded-lg flex-shrink-0" />
                                 <div className="flex flex-col">
                                     <h3 className="font-bold text-lg md:text-xl leading-tight mt-1">Prince of Persia torna sotto i riflettori con il nuovo capitolo della saga</h3>
                                     <p className="text-sm text-gray-300 mt-2 leading-6">La storica serie di Ubisoft è pronta a tornare con nuove meccaniche di gameplay e un rinnovato stile visivo. Il gioco promette di combinare l'azione classica con elementi moderni.</p>
